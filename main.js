80aca73c6d1e09e699c4690ea5860a9a42431141
DeAndre Hoff <deandreryanhoff@yahoo.com>
2021-10-14 10:11:24 -0700

    Done.

--- main.js
+++ main.js
@@ -2,11 +2,31 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+
+const articleHearts = document.querySelectorAll('.like-glyph')
+function likeCallback(e) {
+  const heart = e.target
+  mimicServerCall()
+    .then( function(){
+      if (heart.innerText === EMPTY_HEART){
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      } else {
+        heart.innerText = EMPTY_HEART
+        heart.className = " "
+
+      }
+
+    })
+    .catch()
+
+}
 
 
-
-
+for (const heart of articleHearts) {
+  
+heart.addEventListener("click", likeCallback )
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
